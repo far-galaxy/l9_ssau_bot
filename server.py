667be88257aa26e102ad8407557552a5ceeaf507
far-galaxy <far.galaxy.klg@gmail.com>
2022-08-21 11:14:08 +0300

    added index page

--- server.py
+++ server.py
@@ -1,8 +1,21 @@
 from flask import * 
-app = Flask(__name__) 
+app = Flask(__name__)
+
 @app.route("/") 
 def index():
-   return "Hello l9labs"
+   return send_file("index.html")
+
+@app.route('/<path:path>', methods=['GET'])
+def get_files(path):
+    try:
+        if (path.find("style") != -1 or 
+        path.find("media") != -1 or
+        path.find(".html") != -1):
+            return send_file(path)
+        else:
+            abort(404)
+    except FileNotFoundError:
+        abort(404)
   
 if __name__ == "__main__":
    app.run(debug=True, host='0.0.0.0')
