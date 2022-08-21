3e0868220f09744920a1a0848280ba639024e666
far-galaxy <far.galaxy.klg@gmail.com>
2022-08-21 18:48:33 +0300

    added getiing access token and some info from user

--- server.py
+++ server.py
@@ -1,19 +1,27 @@
-from flask import * 
+from flask import *
+from vkbot import *
+import requests
+
 app = Flask(__name__)
+
+req = {}
+req['client_id'] = VKBot.check_file("settings/client_id")
+req['client_secret'] = VKBot.check_file("settings/client_secret")
+req['redirect_uri'] = VKBot.check_file("settings/redirect_uri")
 
 @app.route("/") 
 def index():
-   return send_file("index.html")
+    return send_file("index.html")
 
 @app.route("/lk") 
 def lk():
-   return send_file("joke.html")
+    return send_file("joke.html")
 
 @app.route('/<path:path>', methods=['GET'])
 def get_files(path):
     try:
         if (path.find("style") != -1 or 
-        path.find("media") != -1 or
+          path.find("media") != -1 or
         path.find(".html") != -1):
             return send_file(path)
         else:
@@ -24,8 +32,19 @@ def get_files(path):
 @app.route('/auth', methods=['GET'])
 def auth():
     code = request.args.get('code')
-    return redirect('/lk')
-   
-  
+    req['code'] = code
+    data = requests.get("https://oauth.vk.com/access_token", params=req)
+    if data.status_code == 200:
+        data = data.json()
+        user_req = {}
+        user_req['v'] = '5.81'
+        user_req['uids'] = data['user_id']
+        user_req['access_token'] = data['access_token']
+        user_req['fields'] = 'photo_big'
+        user_data = requests.get("https://api.vk.com/method/users.get", params=user_req)
+        if user_data.status_code == 200:
+            return str(user_data.json())
+    return "Ошибка"
+
 if __name__ == "__main__":
-   app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
