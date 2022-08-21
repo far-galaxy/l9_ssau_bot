1494759367c307ce7c9b6c0775acbedcd2c10f01
far-galaxy <far.galaxy.klg@gmail.com>
2022-08-21 19:40:56 +0300

    added adding user in sql database

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import *
 from vkbot import *
+from mysql.connector import connect
 import requests
 
 app = Flask(__name__)
@@ -8,6 +9,24 @@ req = {}
 req['client_id'] = VKBot.check_file("settings/client_id")
 req['client_secret'] = VKBot.check_file("settings/client_secret")
 req['redirect_uri'] = VKBot.check_file("settings/redirect_uri")
+
+sql_pass = VKBot.check_file("settings/sql_pass")
+
+database = connect( host = "localhost",
+                    user = 'root',
+                    password = sql_pass)
+
+cursor = database.cursor()
+cursor.execute("USE l9users")
+
+ssau_users = """CREATE TABLE IF NOT EXISTS ssau_users (
+    vkId INTEGER PRIMARY KEY,
+    userName VARCHAR(10),
+    userSurname VARCHAR(10),
+    userPhotoUrl TEXT
+    );"""
+
+cursor.execute(ssau_users)
 
 @app.route("/") 
 def index():
@@ -43,7 +62,24 @@ def auth():
         user_req['fields'] = 'photo_big'
         user_data = requests.get("https://api.vk.com/method/users.get", params=user_req)
         if user_data.status_code == 200:
-            return str(user_data.json())
+            user_data = user_data.json()['response'][0]
+
+            user = [''] * 4
+            user[0] = user_data['id']
+            user[1] = user_data['first_name']
+            user[2] = user_data['last_name']
+            user[3] = user_data['photo_big']
+            print(user)
+            
+            user_query = """
+            INSERT INTO ssau_users
+            (vkId, userName, userSurname, userPhotoUrl)
+            VALUES ( %s, %s, %s, %s)
+            """
+
+            cursor.execute(user_query, tuple(user))
+            database.commit()
+            return redirect("/lk")
     return "Ошибка"
 
 if __name__ == "__main__":
