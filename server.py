ffda5f1a3524a84f524d41ffaabc3bc2db0cf7d6
far-galaxy <far.galaxy.klg@gmail.com>
2022-09-02 19:51:37 +0400

    rewrited all starting with sql utilitiy

--- server.py
+++ server.py
@@ -1,110 +1,14 @@
 # -*- coding: utf-8 -*-
 from flask import *
-from vkbot import *
-from sql import *
 from logger import *
-import requests
-from ast import literal_eval
 
 init_logger()
-logger.info('Logger Ready. Initializating...')
 
 app = Flask(__name__)
 
-req = {}
-req['client_id'] = VKBot.check_file("settings/client_id")
-req['client_secret'] = VKBot.check_file("settings/client_secret")
-req['redirect_uri'] = VKBot.check_file("settings/redirect_uri")
-
-sql_pass = VKBot.check_file("settings/sql_pass")
-
-db = Database("localhost","root",sql_pass)
-
 @app.route("/") 
 def index():
-    return send_file("index.html")
-
-@app.route("/lk") 
-def lk():
-    return send_file("joke.html")
-
-@app.route('/<path:path>', methods=['GET'])
-def get_files(path):
-    try:
-        if (path.find("style") != -1 or 
-          path.find("media") != -1 or
-        path.find(".html") != -1):
-            return send_file(path)
-        else:
-            abort(404)
-    except FileNotFoundError:
-        abort(404)
-
-@app.route('/auth', methods=['GET'])
-def auth():
-    if request.args.get('state') == "vk":
-        code = request.args.get('code')
-        req['code'] = code
-        data = requests.get("https://oauth.vk.com/access_token", params=req)
-        logger.info(data.content)
-        if data.status_code == 200:
-            data = data.json()
-            user = getUser(data)
-            
-            if isinstance(user, list):
-                user_query = """
-                INSERT IGNORE INTO l9_users
-                (l9Id, vkId, userName, userSurname, userPhotoUrl)
-                VALUES (%s, %s, %s, %s, %s)
-                """
-    
-                db.cursor.execute(user_query, user)
-                db.database.commit()         
-                return redirect(f"/lk?l9Id={user[0]}")
-            else:
-                return user.content
-        else:    
-            return data.json()["error_description"]
-        
-    else:
-        abort(501)
-        
-@app.route('/api/lk/users.get', methods=['GET'])
-def usersGet():
-    uid = request.args.get('l9Id')
-    query = """SELECT * FROM l9_users WHERE l9Id = %s"""
-    db.cursor.execute(query, [uid])
-    users = []
-    for user in db.cursor.fetchall():
-        print(user)
-        keys = ['l9Id','vkId','userName','userSurname','userPhotoUrl']
-        users.append(dict(zip(keys, user)))
-    return json.dumps(users, sort_keys = False)
-
-def getUser(data):
-    user_req = {}
-    user_req['v'] = '5.81'
-    user_req['uids'] = data['user_id']
-    user_req['access_token'] = data['access_token']
-    user_req['fields'] = 'photo_big'
-    user_data = requests.get("https://api.vk.com/method/users.get", params=user_req)
-    logger.info(user_data.content)
-    if user_data.status_code == 200:
-        d = user_data.content.decode()
-        user_data = literal_eval(d)['response'][0]
-
-        user = [''] * 5
-        user[0] = db.newID()
-        user[1] = user_data['id']
-        user[2] = user_data['first_name']
-        user[3] = user_data['last_name']
-        user[4] = user_data['photo_big']
-        
-        return user
-    else:
-        return user_data
-        
-  
+	return send_file("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0')
--- sql.py
+++ sql.py
@@ -3,47 +3,65 @@ import random
 
 class Database():
 	def __init__(self, host, user, password):
-		self.database = connect( host = host,
-							user = user,
-							password = password)
-		
+		self.database = connect(host = host,
+								user = user,
+								password = password)
 		self.cursor = self.database.cursor()
-		self.cursor.execute("CREATE DATABASE IF NOT EXISTS l9_lk")
-		self.cursor.execute("USE l9_lk")
+	
+	def exec(self, query, commit = False):
+		self.cursor.execute(query)
+		if commit:
+			self.database.commit()
+		return self.cursor
 		
-		users_table = """CREATE TABLE IF NOT EXISTS l9_users (
-		    l9Id INTEGER PRIMARY KEY,
-			vkId INTEGER,
-			userName TEXT,
-			userSurname TEXT,
-			userPhotoUrl TEXT
-			);"""
+	def initDatabase(self, name):
+		self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {name}")
+		self.cursor.execute(f"USE {name}")		
 		
-		self.cursor.execute(users_table)		
+	def initTable(self, name, head):
+		query = f"CREATE TABLE IF NOT EXISTS {name} ("
+		query += ", ".join([" ".join(i) for i in head])
+		query += ");"
+		self.exec(query)
 		
-	def newID(self):
-		l9ID = random.randint(100000000,999999999)
+	def insert(self, name, values):
+		query = f"INSERT IGNORE INTO {name} ("
+		query += ", ".join(values) + ") VALUES ("
+		query += ", ".join(values.values()) + ");"
+		self.exec(query, commit = True)
 		
-		check_id = """SELECT l9Id FROM l9_users
-		WHERE l9Id = %s"""
+	def get(self, name, condition, columns = None):
+		query = "SELECT " + (', '.join(columns) if columns != None else "*")
+		query += f" FROM {name} WHERE {condition};"
+		return self.exec(query)
 		
-		self.cursor.execute(check_id, tuple([l9ID]))
-		exist = self.cursor.fetchall() != []
+	def newID(self, name, id_name):
+		someID = random.randint(100000000,999999999)
+		
+		result = self.get(name, f"{id_name} = {someID}")
+		
+		exist = result.fetchall() != []
 		if not exist:
-			return l9ID
+			return str(someID)
 		else:
-			self.newID()
-			
+			self.newID()	
+		
 if __name__ == "__main__":
 	from vkbot import VKBot
 	sql_pass = VKBot.check_file("settings/sql_pass")
 	db = Database("localhost","root",sql_pass)
+	db.initDatabase("l9_lk")
+	db.initTable("l9_users", [
+		["l9Id", "INTEGER", "PRIMARY KEY"],
+		["vkId", "INTEGER"]
+	])
 	
-	user_query = """
-            INSERT IGNORE INTO l9_users
-            (l9Id, userName, userSurname)
-            VALUES (%s, 'test', 'test')
-            """
-
-	db.cursor.execute(user_query, [db.newID()])
-	db.database.commit()
+	db.insert("l9_users", {
+		"l9Id":db.newID("l9_users", "l9Id"), 
+		"vkId":"5678"
+	})
+	
+	db.get("l9_users", 
+		   "l9Id = 1", 
+		   ["l9Id, vkId"]
+		   )
